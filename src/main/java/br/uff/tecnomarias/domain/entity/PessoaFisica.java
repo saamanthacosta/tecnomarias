bf8fdb0ace55d6f0da74f1a82ee3cf7adbc37413
Luisa Esposito <a.luisa.esposito@gmail.com>
2021-03-10 19:37:42 -0300

    Corrige pf

--- src/main/java/br/uff/tecnomarias/domain/entity/PessoaFisica.java
+++ src/main/java/br/uff/tecnomarias/domain/entity/PessoaFisica.java
@@ -1,35 +1,17 @@
 package br.uff.tecnomarias.domain.entity;
 
 import javax.persistence.*;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class PessoaFisica extends Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Column(unique = true)
-    @Size(min = 11, max = 11, message = "CPF deve ter 11 caracteres")
-    private String cpf;
-
     @OneToOne(cascade = CascadeType.ALL)
     @Column(name = "id_links")
     private Links links;
 
-    @OneToMany(mappedBy = "avaliadora", cascade = CascadeType.REMOVE)
-    private List<Avaliacao> avaliacoes = new ArrayList<>();
-
     public PessoaFisica() {
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
     }
 
     public Links getLinks() {
