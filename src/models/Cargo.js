d5d24276969e0ccdd8c667c40c90217245947396
Samantha <samantha_apcosta@hotmail.com>
2021-04-13 23:27:41 -0300

    [ADD] Criação das entidades

---
+++ src/models/Cargo.js
@@ -0,0 +1,7 @@
+export const Cargo = {
+    ESTAGIARIA: 'Estagiária',
+    JUNIOR: 'Júnior',
+    PLENO: 'Pleno',
+    SENIOR: 'Senior',
+    GERENTE: 'Gerente'
+}
---
+++ src/models/PorteEmpresa.js
@@ -0,0 +1,6 @@
+export const PorteEmpresa = {
+    MICROEMPRESA: 'Microempresa',
+    EMPRESA_PEQUENO_PORTE: 'Pequeno porte',
+    EMPRESA_MEDIO_PORTE: 'Médio porte',
+    GRANDE_EMPRESA: 'Grande empresa'
+}
