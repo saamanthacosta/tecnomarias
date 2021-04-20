7c9c9570b3b405dc750bdf2a70b1970a22bdf50b
Samantha <samantha_apcosta@hotmail.com>
2021-04-20 20:11:40 -0300

    [ADD] Input tipo radio + select atualizado

--- src/components/common/Input/Select/index.jsx
+++ src/components/common/Input/Select/index.jsx
@@ -1,33 +1,21 @@
 import React from 'react'
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
 import PropTypes from 'prop-types';
-import { FormControl, FormHelperText } from '@material-ui/core';
+import { FormControl, FormHelperText, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
 
-export default function InputSelect({ label, value, onChange, opcoes, textoDeAjuda, erro, id, required, grande }) {
+export default function InputSelect({ label, value, onChange, opcoes, textoDeAjuda, required, grande }) {
     return <>
-
-        <FormControl required={required} fullWidth={grande} error={erro}>
-            <InputLabel id={id}>{label}</InputLabel>
-            <Select
-                labelId={id}
+        <FormControl required={required} fullWidth={grande}>
+            <Autocomplete
                 value={value}
                 onChange={onChange}
-                required={true}
-                fullWidth={true}
-                renderValue={(value) => value}
-            >
-                {
-                    opcoes.map(opcao => {
-                        return (
-                            <MenuItem key={opcao.value} value={opcao.value}>{opcao.nome}</MenuItem>
-                        )
-                    })
-                }
-            </Select>
+                options={opcoes}
+                getOptionLabel={(option) => option.nome}
+                renderInput={(params) => <TextField {...params} label={label} />}
+            />
             <FormHelperText>{textoDeAjuda}</FormHelperText>
         </FormControl>
+
     </>
 }
 
@@ -36,17 +24,14 @@ InputSelect.propTypes = {
     label: PropTypes.string.isRequired,
     textoDeAjuda: PropTypes.string,
     value: PropTypes.any.isRequired,
-    id: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
     opcoes: PropTypes.array.isRequired,
     required: PropTypes.bool,
-    grande: PropTypes.bool,
-    erro: PropTypes.bool
+    grande: PropTypes.bool
 }
 
 InputSelect.defaultProps = {
     required: false,
     grande: true,
-    erro: false,
     textoDeAjuda: null
 }
