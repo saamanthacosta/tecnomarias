978650cf573eb7e90b3743cde882256d2a58c615
Samantha <samantha_apcosta@hotmail.com>
2021-04-20 20:20:29 -0300

    [DELETE] Merge pull request #2 from saamanthacosta/feature/inputs
    Feature/inputs

---
+++ src/components/common/Input/Select/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { FormControl, FormHelperText, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+
+export default function InputSelect({ label, value, onChange, opcoes, textoDeAjuda, required, grande }) {
+    return <>
+        <FormControl required={required} fullWidth={grande}>
+            <Autocomplete
+                value={value}
+                onChange={onChange}
+                options={opcoes}
+                getOptionLabel={(option) => option.nome}
+                renderInput={(params) => <TextField {...params} label={label} />}
+            />
+            <FormHelperText>{textoDeAjuda}</FormHelperText>
+        </FormControl>
+
+    </>
+}
+
+
+InputSelect.propTypes = {
+    label: PropTypes.string.isRequired,
+    textoDeAjuda: PropTypes.string,
+    value: PropTypes.any.isRequired,
+    onChange: PropTypes.func.isRequired,
+    opcoes: PropTypes.array.isRequired,
+    required: PropTypes.bool,
+    grande: PropTypes.bool
+}
+
+InputSelect.defaultProps = {
+    required: false,
+    grande: true,
+    textoDeAjuda: null
+}
--- src/components/common/Modais/Confirmar/Existencia-Conta/index.jsx
+++ src/components/common/Modais/Confirmar/Existencia-Conta/index.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from 'react';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import InputTexto from '../../../Input/Texto'
 import Modal from '../..';
 
 export default function ConfirmarCadastro() {
 
+  const [email, setEmail] = useState("");
+
   const conteudoModal = <>
-    <DialogContentText id="alert-dialog-description">
+    <DialogContentText >
       Antes de você realizar o cadastro, vamos confirmar se você já não tem um e-mail cadastrado com a gente! Insira seu e-mail no campo abaixo.
     </DialogContentText>
-    <TextField
-      autoFocus
-      margin="dense"
-      name="email"
-      label="Email"
+    <InputTexto
       type="email"
-      fullWidth
+      label="Email"
+      grande={true}
+      required={true}
+      onChange={(e) => setEmail(e.target.value)}
+      textoDeAjuda="Insira o e-mail que você iria realizar cadastro conosco"
+      value={email}
     />
   </>
 
   const modal = {
     titulo: {
       nome: 'Tem certeza que é novo por aqui?',
-      avatar: <CheckCircleOutlineIcon color="secondary" fontSize="large" />
+      avatar: <CheckCircleOutlineIcon color="primary" fontSize="large" />
     },
     conteudo: conteudoModal,
     botao: {
       abrir: {
         nome: 'Login',
-        cor: 'primary'
       },
       acaoPrincipal: {
         nome: 'Verificar',
