6e53e804b2a3e63e3fc58455f90670da2823e258
Samantha <samantha_apcosta@hotmail.com>
2021-04-21 13:26:00 -0300

    [FIX] voltar do formulario

--- src/components/common/Formulario/index.jsx
+++ src/components/common/Formulario/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import BotaoSimples from '../Botao/Simples'
+import PropTypes from 'prop-types';
+import { useHistory } from "react-router-dom";
 
 export default function Formulario({ corpo, autoComplete, noValidate, botao }) {
+
+    let history = useHistory();
 
     return <>
         <form autoComplete={autoComplete} noValidate={noValidate} onSubmit={botao.funcao} >
             {corpo}
             <BotaoSimples
             nome="Voltar"
-            onClick={this.context.router.history.goBack}
+            onClick={history.goBack}
             cor="primary"
             />
             <BotaoSimples
@@ -19,4 +23,15 @@ export default function Formulario({ corpo, autoComplete, noValidate, botao }) {
             />
         </form>
     </>
+}
+
+InputTexto.propTypes = {
+    corpo: PropTypes.element.isRequired,
+    autoComplete: PropTypes.bool,
+    noValidate: PropTypes.bool,
+}
+
+InputTexto.defaultProps = {
+    autoComplete: false,
+    noValidate: false
 }
