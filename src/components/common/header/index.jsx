5b673c43934411cfa143fdc476c7e3b0cdf2cc97
Jose <josejunior@id.uff.br>
2021-04-30 18:09:32 -0300

    navbar montada

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,18 +1,18 @@
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import logo from "../../../assets/imagens/logo-header.png"
+import logo from "../../../assets/imagens/logo-header2.PNG";
 
 
 export default function Header() {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Navbar.Brand href="#home">
       <img
         alt="logo-header"
         src={logo}
-        width="180"
-        height="35"
+        width="170"
+        height="50"
         className="d-inline-block align-top"
-      />
+      /> 
     </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -22,8 +22,12 @@ export default function Header() {
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#vagas">Vagas</Nav.Link>
                     <Nav.Link href="#cadastros">Cadastros</Nav.Link>
+                    <NavDropdown title="Conecte-se" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/loginpf">Login P. Física</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/loginpj">Login P. Jurídica</NavDropdown.Item>
+                    </NavDropdown>
                     <Nav.Link href="#sobre">Sobre</Nav.Link>
-                    <Nav.Link href="#conecte-se">Conecte-se</Nav.Link>
+                    
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
