1b4d6f2a03db816377d95292007f23457b2f4491
Samantha <samantha_apcosta@hotmail.com>
2021-05-01 02:37:40 -0300

    na vdd agora que vai

--- src/services/Request.js
+++ src/services/Request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 var API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "http://tecnomarias.herokuapp.com/api/v1/",
     headers: {
         'Content-Type': 'application/json',
     }
