600b339c09eabe14af175a2b03bac6c747db4bce
Samantha <samantha_apcosta@hotmail.com>
2021-05-02 03:31:34 -0300

    [FIX] tamanho da pagina

--- src/components/common/Page/index.jsx
+++ src/components/common/Page/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Page({ children }) {
     return (
-        <div style={{minHeight: window.innerHeight - 320, backgroundColor: '#ffffff', padding: '40px 0px'}}>
+        <div style={{minHeight: window.innerHeight - 300, backgroundColor: '#ffffff', padding: '40px 0px'}}>
             {children}
         </div>
     )
