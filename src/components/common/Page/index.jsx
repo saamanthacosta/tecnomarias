7d65b1c706e438b1e0d971099eb422405a233499
Samantha <samantha_apcosta@hotmail.com>
2021-05-02 03:22:41 -0300

    [FIX] Background preto

--- src/components/common/Page/index.jsx
+++ src/components/common/Page/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import '../../../assets/css/pagina.css'
 
 export default function Page({ children }) {
-
     return (
-        <div className="pagina" id="pagina" style={{minHeight: window.innerHeight - 320}}>
+        <div style={{minHeight: window.innerHeight - 320, backgroundColor: '#ffffff', padding: '40px 0px'}}>
             {children}
         </div>
     )
-
 }
